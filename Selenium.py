a785e216503539dc8fcfc04fe5ceaa1e4609f673
Artyr96 <gerasimovprivet@gmail.com>
2020-06-21 14:18:07 +0300

    Добавлен новый класс

--- Selenium.py
+++ Selenium.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from new_user import New_user
 
 
 class AppDynamicsJob(unittest.TestCase):
@@ -17,7 +18,7 @@ class AppDynamicsJob(unittest.TestCase):
         driver = self.driver
         driver.get("https://starkportal.solardigital.com.ua/login")
         self.login(driver, username="artur", password="artur")
-        self.add_user(driver, name="te1", username="te1", password="321")
+        self.add_user(driver, New_user(name="te1", username="te1", password="321"))
         #logout
         driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/div/div[2]/div/div/div/div").click()
         driver.find_element_by_xpath("//button[@type='submit']").click()
@@ -26,24 +27,24 @@ class AppDynamicsJob(unittest.TestCase):
         driver = self.driver
         driver.get("https://starkportal.solardigital.com.ua/login")
         self.login(driver, username="artur", password="artur")
-        self.add_user(driver, name="te2", username="te2", password="321")
+        self.add_user(driver, New_user(name="te2", username="te2", password="321"))
         #logout
         driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/div/div[2]/div/div/div/div").click()
         driver.find_element_by_xpath("//button[@type='submit']").click()
 
-    def add_user(self, driver, name, username, password):
+    def add_user(self, driver, new_user):
         driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/nav/ul/li[4]/button/span").click()
         driver.find_element_by_link_text("Users").click()
         driver.find_element_by_xpath("//div[@id='app']/header/div/div/button").click()
         driver.find_element_by_name("name").click()
         driver.find_element_by_name("name").clear()
-        driver.find_element_by_name("name").send_keys(name)
+        driver.find_element_by_name("name").send_keys(new_user.name)
         driver.find_element_by_name("username").click()
         driver.find_element_by_name("username").clear()
-        driver.find_element_by_name("username").send_keys(username)
+        driver.find_element_by_name("username").send_keys(new_user.username)
         driver.find_element_by_name("password").click()
         driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys(password)
+        driver.find_element_by_name("password").send_keys(new_user.password)
         driver.find_element_by_xpath(
             "//div[@id='app']/main/form/div/div[2]/div/div[4]/div/div[2]/div/div/div/div/div").click()
         driver.find_element_by_xpath(
---
+++ new_user.py
@@ -0,0 +1,5 @@
+class New_user:
+    def __init__(self, name, username, password):
+        self.name = name
+        self.username = username
+        self.password = password
