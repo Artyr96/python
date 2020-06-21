4371833d637e7a369c37ddf18c0507c9517c2ec9
Artyr96 <gerasimovprivet@gmail.com>
2020-06-22 00:37:55 +0300

    Перенос в отдельный класс

--- Selenium.py
+++ Selenium.py
@@ -1,98 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
 from new_user import New_user
+from application import Application
+import pytest
 
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class AppDynamicsJob(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+def test_app_dynamics_job_2(app):
+    app.login(username="artur", password="artur")
+    app.add_user( New_user(name="te2", username="te2", password="321"))
+    app.logout()
 
-    def open_page(self, driver):
-        driver.get("https://starkportal.solardigital.com.ua/login")
+def test_app_dynamics_job_1(app):
+    app.login(username="artur", password="artur")
+    app.add_user(New_user(name="te1", username="te1", password="321"))
+    app.logout()
 
-    def add_user(self, driver, new_user):
-        driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/nav/ul/li[4]/button/span").click()
-        driver.find_element_by_link_text("Users").click()
-        driver.find_element_by_xpath("//div[@id='app']/header/div/div/button").click()
-        driver.find_element_by_name("name").click()
-        driver.find_element_by_name("name").clear()
-        driver.find_element_by_name("name").send_keys(new_user.name)
-        driver.find_element_by_name("username").click()
-        driver.find_element_by_name("username").clear()
-        driver.find_element_by_name("username").send_keys(new_user.username)
-        driver.find_element_by_name("password").click()
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys(new_user.password)
-        driver.find_element_by_xpath(
-            "//div[@id='app']/main/form/div/div[2]/div/div[4]/div/div[2]/div/div/div/div/div").click()
-        driver.find_element_by_xpath(
-            "//div[@id='app']/main/form/div/div[2]/div/div[4]/div/div[2]/div/div/div/div/div[3]/ul/li/span").click()
-        driver.find_element_by_xpath("(//button[@type='submit'])[2]").click()
-
-    def login(self, driver, username, password):
-        driver.find_element_by_name("username").click()
-        driver.find_element_by_name("username").clear()
-        driver.find_element_by_name("username").send_keys(username)
-        driver.find_element_by_xpath("//div/div").click()
-        driver.find_element_by_name("password").click()
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys(password)
-        driver.find_element_by_xpath("//button[@type='submit']").click()
-
-    def logout(self, driver):
-        driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/div/div[2]/div/div/div/div").click()
-        driver.find_element_by_xpath("//button[@type='submit']").click()
-
-    def test_app_dynamics_job_2(self):
-        driver = self.driver
-        self.open_page(driver)
-        self.login(driver, username="artur", password="artur")
-        self.add_user(driver, New_user(name="te2", username="te2", password="321"))
-        self.logout(driver)
-
-    def test_app_dynamics_job_1(self):
-        driver = self.driver
-        self.open_page(driver)
-        self.login(driver, username="artur", password="artur")
-        self.add_user(driver, New_user(name="te1", username="te1", password="321"))
-        self.logout(driver)
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        self.assertEqual([], self.verificationErrors)
-
-
-if __name__ == "__main__":
-    unittest.main()
---
+++ application.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+
+class Application:
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(60)
+
+    def open_page(self):
+        driver = self.driver
+        driver.get("https://starkportal.solardigital.com.ua/login")
+
+    def login(self, username, password):
+        driver = self.driver
+        self.open_page()
+        driver.find_element_by_name("username").click()
+        driver.find_element_by_name("username").clear()
+        driver.find_element_by_name("username").send_keys(username)
+        driver.find_element_by_xpath("//div/div").click()
+        driver.find_element_by_name("password").click()
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys(password)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+
+    def add_user(self, new_user):
+        driver = self.driver
+        driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/nav/ul/li[4]/button/span").click()
+        driver.find_element_by_link_text("Users").click()
+        driver.find_element_by_xpath("//div[@id='app']/header/div/div/button").click()
+        driver.find_element_by_name("name").click()
+        driver.find_element_by_name("name").clear()
+        driver.find_element_by_name("name").send_keys(new_user.name)
+        driver.find_element_by_name("username").click()
+        driver.find_element_by_name("username").clear()
+        driver.find_element_by_name("username").send_keys(new_user.username)
+        driver.find_element_by_name("password").click()
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys(new_user.password)
+        driver.find_element_by_xpath(
+            "//div[@id='app']/main/form/div/div[2]/div/div[4]/div/div[2]/div/div/div/div/div").click()
+        driver.find_element_by_xpath(
+            "//div[@id='app']/main/form/div/div[2]/div/div[4]/div/div[2]/div/div/div/div/div[3]/ul/li/span").click()
+        driver.find_element_by_xpath("(//button[@type='submit'])[2]").click()
+
+    def logout(self):
+        driver = self.driver
+        driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/div/div[2]/div/div/div/div").click()
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+
+    def destroy(self):
+        self.driver.quit()
