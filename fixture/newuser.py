2c68dd39031577df4e41295dc93e6cbf8f1b1faf
Artyr96 <gerasimovprivet@gmail.com>
2020-06-29 22:37:36 +0300

    Не рабочий новый тест на изменение

--- fixture/newuser.py
+++ fixture/newuser.py
@@ -1,37 +1,46 @@
 class NuserHelper:
 
-
     def __init__(self, app):
         self.app = app
 
-    def open_page(self):
+    def add(self, new_user_data):
         driver = self.app.driver
-        driver.get("https://starkportal.solardigital.com.ua/login")
-
-    def add(self, new_user):
-        driver = self.app.driver
+        self.app.open_page()
         driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/nav/ul/li[4]/button/span").click()
         driver.find_element_by_link_text("Users").click()
         driver.find_element_by_xpath("//div[@id='app']/header/div/div/button").click()
-        driver.find_element_by_name("name").click()
-        driver.find_element_by_name("name").clear()
-        driver.find_element_by_name("name").send_keys(new_user.name)
-        driver.find_element_by_name("username").click()
-        driver.find_element_by_name("username").clear()
-        driver.find_element_by_name("username").send_keys(new_user.username)
-        driver.find_element_by_name("password").click()
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys(new_user.password)
-        driver.find_element_by_xpath(
-            "//div[@id='app']/main/form/div/div[2]/div/div[4]/div/div[2]/div/div/div/div/div").click()
-        driver.find_element_by_xpath(
-            "//div[@id='app']/main/form/div/div[2]/div/div[4]/div/div[2]/div/div/div/div/div[3]/ul/li/span").click()
+        self.fill_user_form(new_user_data)
+        driver.find_element_by_xpath("//div[@id='app']/main/form/div/div[2]/div/div[4]/div/div[2]/div/div/div/div/div").click()
+        driver.find_element_by_xpath("//div[@id='app']/main/form/div/div[2]/div/div[4]/div/div[2]/div/div/div/div/div[3]/ul/li/span").click()
         driver.find_element_by_xpath("(//button[@type='submit'])[2]").click()
+
+    def change_field(self, field_name, text):
+        driver = self.app.driver
+        if text is not None:
+            driver.find_element_by_name(field_name).click()
+            driver.find_element_by_name(field_name).clear()
+            driver.find_element_by_name(field_name).send_keys(text)
+
+    def fill_user_form(self, new_user):
+        self.change_field("name", new_user.name)
+        self.change_field("username", new_user.username)
+        self.change_field("password", new_user.password)
 
     def delete_user(self):
         driver = self.app.driver
         self.app.open_page()
         driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/nav/ul/li[4]/button/span").click()
         driver.find_element_by_link_text("Users").click()
-        driver.find_element_by_css_selector("#gen-table-row-15 > div.gen-table-group.gen-table-group--actions > div.gen-table-cell.gen-table-cell__actions > div.btn-gen-table-delete-wrap > button.btn-gen-table-delete > svg").click()
+        driver.find_element_by_css_selector(
+            "#gen-table-row-15 > div.gen-table-group.gen-table-group--actions > div.gen-table-cell.gen-table-cell__actions > div.btn-gen-table-delete-wrap > button.btn-gen-table-delete > svg").click()
         driver.find_element_by_xpath("//div[@id='gen-table-row-15']/div[4]/div/div[3]/button/div/button[2]").click()
+
+    def modify(self, new_user_data):
+        driver = self.app.driver
+        self.app.open_page()
+        driver.find_element_by_xpath("//div[@id='app']/aside/div[2]/nav/ul/li[4]/button/span").click()
+        driver.find_element_by_link_text("Users").click()
+        driver.find_element_by_css_selector(
+            "#gen-table-row-15>div.gen-table-group.gen-table-group--actions>div.gen-table-cell.gen-table-cell__actions > div.btn-gen-table-edit-wrap > button.btn-gen-table-edit > svg").click()
+        self.fill_user_form(new_user_data)
+        driver.find_element_by_xpath("//*[@id='app']/main/form[2]/div/div[1]/div[3]/button").click()
--- model/new_user.py
+++ model/new_user.py
@@ -1,5 +1,5 @@
 class New_user:
-    def __init__(self, name, username, password):
+    def __init__(self, name=None, username=None, password=None):
         self.name = name
         self.username = username
         self.password = password
